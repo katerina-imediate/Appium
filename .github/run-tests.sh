8adeb501bb50da3c0058b4d9916dcd442e12cfcf
katerina-imediate <katexrg@yahoo.com>
2025-01-04 22:25:16 +0200

    u

--- .github/run-tests.sh
+++ .github/run-tests.sh
@@ -21,7 +21,7 @@ checkTestPrerequisites() {
     fi
 
     # Check if android-apidemos is installed
-    if [ ! -f "node_modules/android-apidemos/apks/ApiDemos-debug.apk" ]; then
+    if [ ! -f "apps/android.wdio.native.app.v1.0.8.apk" ]; then
         echo "Error: android-apidemos package not found"
         echo "Installing android-apidemos package..."
         npm install android-apidemos --save-dev
@@ -30,20 +30,15 @@ checkTestPrerequisites() {
 
 checkTestPrerequisites
 
-RESULTS_XML=test-results.xml
-echo "{\"reporterEnabled\": \"spec, xunit\", \"xunitReporterOptions\": {\"output\": \"$RESULTS_XML\"}}" > reporter_config.json
-ARGS=(./test/functional/driver-e2e-specs.js \
-./test/functional/commands \
-./test/functional/commands/find \
-./test/functional/commands/general \
-./test/functional/commands/keyboard \
+#RESULTS_XML=test-results.xml
+#echo "{\"reporterEnabled\": \"spec, xunit\", \"xunitReporterOptions\": {\"output\": \"$RESULTS_XML\"}}" > reporter_config.json
+ARGS=(./tests/specs/ \
+
 --exit --timeout 10m \
---reporter mocha-multi-reporters --reporter-options configFile=reporter_config.json)
+--reporter mocha-multi-reporters 
 if ! npx mocha "${ARGS[@]}"; then
-  tests=$(cat "$RESULTS_XML" | xq --xpath '//testsuite/@tests')
-  errors=$(cat "$RESULTS_XML" | xq --xpath '//testsuite/@errors')
-  skipped=$(cat "$RESULTS_XML" | xq --xpath '//testsuite/@skipped')
-  failures=$(cat "$RESULTS_XML" | xq --xpath '//testsuite/@failures')
+  tests=$(cat "$RESULTS_XML" | xq --xpath '//tests')
+ 
   threshold=$(( (failures + errors) * 100 / (tests - skipped) ))
   cat "$RESULTS_XML"
   if [[ $threshold -gt $TEST_PASS_THRESHOLD ]]; then
